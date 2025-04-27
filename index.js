34c547ab69bee24cbbead11e56c31b953af521d4
Muhamed Razik <muhammedrazik987@gmail.com>
2025-04-27 11:16:56 +0530

    fix: prevent grid creation when custom size input is empty, cancelled, or out of range

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ let isRedActive = false;
 let isBlueActive = false;
 let isGreenActive = false;
 
-let activeColor ="black";
+let activeColor = "black";
 
 console.log(container)
 //color selection
-blackBtn.addEventListener('click',() => {
-    isBlackActive = !isBlackActive;  
-    if(isBlackActive) {
+blackBtn.addEventListener('click', () => {
+    isBlackActive = !isBlackActive;
+    if (isBlackActive) {
         blackBtn.style.border = '3px solid grey';
         redBtn.style.border = '3px solid red';
         blueBtn.style.border = '3px solid blue';
@@ -31,7 +31,7 @@ blackBtn.addEventListener('click',() => {
         isGreenActive = false;
         activeColor = "black";
         return activeColor;
-        
+
 
     }
     else {
@@ -39,9 +39,9 @@ blackBtn.addEventListener('click',() => {
     }
 
 });
-redBtn.addEventListener('click',() => {
-    isRedActive = !isRedActive;  
-    if(isRedActive) {
+redBtn.addEventListener('click', () => {
+    isRedActive = !isRedActive;
+    if (isRedActive) {
         blackBtn.style.border = '3px solid black';
         redBtn.style.border = '3px solid grey';
         blueBtn.style.border = '3px solid blue';
@@ -51,48 +51,48 @@ redBtn.addEventListener('click',() => {
         isGreenActive = false;
         activeColor = "red";
         return activeColor;
-    } 
+    }
     else {
-        redBtn.style.border = '3px solid red'; 
+        redBtn.style.border = '3px solid red';
 
     }
-    
+
 });
-blueBtn.addEventListener('click',() => {
-    isBlueActive = !isBlueActive; 
-    if(isBlueActive) {
-        blueBtn.style.border = '3px solid grey'; 
+blueBtn.addEventListener('click', () => {
+    isBlueActive = !isBlueActive;
+    if (isBlueActive) {
+        blueBtn.style.border = '3px solid grey';
         blackBtn.style.border = '3px solid black';
-        redBtn.style.border = '3px solid red'; 
-        greenBtn.style.border = '3px solid green'; 
+        redBtn.style.border = '3px solid red';
+        greenBtn.style.border = '3px solid green';
         isRedActive = false;
         isBlackActive = false;
         isGreenActive = false;
         activeColor = "blue";
         return activeColor;
-          
+
     }
     else {
-        blueBtn.style.border = '3px solid blue'; 
-    }
-    
-    
-});
-greenBtn.addEventListener('click',() => {
-    isGreenActive = !isGreenActive;  
-    if(isGreenActive) {
-        blackBtn.style.border = '3px solid black';
-        redBtn.style.border = '3px solid red'; 
         blueBtn.style.border = '3px solid blue';
-        greenBtn.style.border = '3px solid grey'; 
+    }
+
+
+});
+greenBtn.addEventListener('click', () => {
+    isGreenActive = !isGreenActive;
+    if (isGreenActive) {
+        blackBtn.style.border = '3px solid black';
+        redBtn.style.border = '3px solid red';
+        blueBtn.style.border = '3px solid blue';
+        greenBtn.style.border = '3px solid grey';
         isRedActive = false;
         isBlueActive = false;
         isBlackActive = false;
         activeColor = "green";
-        return activeColor;  
+        return activeColor;
     }
     else {
-        greenBtn.style.border = '3px solid green'; 
+        greenBtn.style.border = '3px solid green';
     }
 });
 
@@ -102,76 +102,76 @@ let isOpacityActive = false;
 let isLeftButtonPressed = false;
 let isRightButtonPressed = false;
 
-hoverBtn.addEventListener('click',() => {
-    isHoverActive = !isHoverActive;  
-   
-    hoverBtn.style.color = isHoverActive ?'white' :'grey';
-    hoverBtn.style.backgroundColor = isHoverActive ?'grey' :'white';
-    hoverBtn.textContent= isHoverActive ?'Button' :'Hover';
+hoverBtn.addEventListener('click', () => {
+    isHoverActive = !isHoverActive;
+
+    hoverBtn.style.color = isHoverActive ? 'white' : 'grey';
+    hoverBtn.style.backgroundColor = isHoverActive ? 'grey' : 'white';
+    hoverBtn.textContent = isHoverActive ? 'Button' : 'Hover';
 
 });
-opacityBtn.addEventListener('click',() => {
-    isOpacityActive = !isOpacityActive;  
-   
-    opacityBtn.style.color = isOpacityActive ?'white' :'grey';
-    opacityBtn.style.backgroundColor = isOpacityActive ?'grey' :'white';
+opacityBtn.addEventListener('click', () => {
+    isOpacityActive = !isOpacityActive;
+
+    opacityBtn.style.color = isOpacityActive ? 'white' : 'grey';
+    opacityBtn.style.backgroundColor = isOpacityActive ? 'grey' : 'white';
 });
 
-document.addEventListener("mousedown",(event)=> {
-    if(event.button ===0) {
+document.addEventListener("mousedown", (event) => {
+    if (event.button === 0) {
         isLeftButtonPressed = true;
     }
-    else if(event.button ===2) {
+    else if (event.button === 2) {
         isRightButtonPressed = true;
     }
 });
-document.addEventListener("mouseup",()=> {
+document.addEventListener("mouseup", () => {
     isLeftButtonPressed = false;
     isRightButtonPressed = false;
 });
 
 // custom Grid without changing total pixel//
-customGrid.addEventListener('click',pixelValue);
+customGrid.addEventListener('click', pixelValue);
 
 function pixelValue() {
     let gridValue = prompt("Enter a number of Squares per Side (MAX = 100)");
     gridValue = parseInt(gridValue);
 
-    if (gridValue == "" || gridValue <1||gridValue > 100) {
+    if (isNaN(gridValue) || gridValue < 1 || gridValue > 100) {
         alert("Please Enter a number between 1 - 100")
         return;
     }
     createGrid(gridValue)
 };
 
-function createGrid (gridValue) {
+function createGrid(gridValue) {
     container.innerHTML = "";
     const containerWidth = 448;
 
-    const pixelSize = (containerWidth/gridValue)-2;
+    const pixelSize = (containerWidth / gridValue) - 2;
 
-    for(i=1; i <= gridValue * gridValue; i++) {
+    for (i = 1; i <= gridValue * gridValue; i++) {
         const divs = document.createElement('div');
         divs.id = 'gridDiv';
         divs.style.width = `${pixelSize}px`;
         divs.style.height = `${pixelSize}px`;
 
-        divs.addEventListener("mouseover",()=> {
-            if(isHoverActive){
-                if(isLeftButtonPressed) {
+        divs.addEventListener("mouseover", () => {
+            if (isHoverActive) {
+                if (isLeftButtonPressed) {
                     divs.style.backgroundColor = activeColor;
-                    if(isOpacityActive) {
+                    if (isOpacityActive) {
                         let currentOpacity = parseFloat(divs.style.opacity) || 0;
                         if (currentOpacity < 1) {
                             currentOpacity += 0.1;
                             divs.style.opacity = currentOpacity.toFixed(1)
                         }
-                    } 
+                    }
                 }
             }
             else {
                 divs.style.backgroundColor = activeColor;
-                if(isOpacityActive) {
+                if (isOpacityActive) {
                     let currentOpacity = parseFloat(divs.style.opacity) || 0;
                     if (currentOpacity < 1) {
                         currentOpacity += 0.1;
@@ -179,13 +179,13 @@ function createGrid (gridValue) {
                     }
                 }
             }
-            if(isRightButtonPressed) {
-            divs.style.backgroundColor = 'white';
+            if (isRightButtonPressed) {
+                divs.style.backgroundColor = 'white';
             }
         });
-     
-         container.appendChild(divs);
-     };
+
+        container.appendChild(divs);
+    };
 }
 // declaring default grid
 createGrid(16)
@@ -194,24 +194,24 @@ const btn16 = document.getElementById('16-size');
 const btn32 = document.getElementById('32-size');
 const btn64 = document.getElementById('64-size');
 
-btn16.addEventListener('click',() => createGrid(16));
-btn32.addEventListener('click',() => createGrid(32));
-btn64.addEventListener('click',() => createGrid(64));
+btn16.addEventListener('click', () => createGrid(16));
+btn32.addEventListener('click', () => createGrid(32));
+btn64.addEventListener('click', () => createGrid(64));
 
 // border activationg and deactivating button
 let isBorderActive = true;
-borderBtn.addEventListener('click',() => {
+borderBtn.addEventListener('click', () => {
     isBorderActive = !isBorderActive;
-    
-    borderBtn.style.color = isBorderActive ?'white' :'grey';
-    borderBtn.style.backgroundColor = isBorderActive ?'grey' :'white';
+
+    borderBtn.style.color = isBorderActive ? 'white' : 'grey';
+    borderBtn.style.backgroundColor = isBorderActive ? 'grey' : 'white';
 
     const divs = document.querySelectorAll('#gridDiv');
 
     divs.forEach(div => {
         div.style.border = isBorderActive
-        ? '1px solid rgba(128, 128, 128, 0.263)'
-        : '1px solid rgba(128, 128, 128, 0)';
+            ? '1px solid rgba(128, 128, 128, 0.263)'
+            : '1px solid rgba(128, 128, 128, 0)';
     });
-    
+
 });
